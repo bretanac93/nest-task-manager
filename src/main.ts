f164f66ea6e319ed29a4fb18486972c8f3fa5d58
Cesar Bretana Gonzalez <bretanac@gmail.com>
2019-10-04 17:08:39 +0200

    Playing with logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(port = 3000) {
+  const logger = new Logger('AppCore');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
