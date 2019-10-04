c2d1f4ec8d1fff43a586f3f27b53cb2e8f6acb94
Cesar Bretana Gonzalez <bretanac@gmail.com>
2019-10-04 16:20:07 +0200

    Using auth module to guard tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,18 +9,20 @@ import {
   Query,
   UsePipes,
   ValidationPipe,
-  NotFoundException,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 
 import { Task } from './task.entity';
-import { TaskStatus } from './task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
 import { TasksService } from './tasks.service';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
